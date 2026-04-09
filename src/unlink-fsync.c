c4e03688706d008211c8bb9faeef8fbab3a45615
Filipe Manana <fdmanana@suse.com>
2026-04-09 16:22:27 +0100

    generic: test fsync against a removed directory
    Test that if we fsync a directory that became empty after removing it
    and we have a power failure, we are able to mount the filesystem and the
    directory does not exists anymore.
    
    This exercises a bug found in btrfs which is fixed by the following patch:
    
      btrfs: fix missing last_unlink_trans update when removing a directory
    
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Zorro Lang <zlang@kernel.org>

--- src/unlink-fsync.c
+++ src/unlink-fsync.c
@@ -4,10 +4,12 @@
  */
 
 /*
- * Utility to open an existing file, unlink it while holding an open fd on it
- * and then fsync the file before closing the fd.
+ * Utility to open an existing file or directory, unlink or rmdir it while
+ * holding an open fd on it and then fsync it before closing the fd.
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,24 +17,43 @@
 
 int main(int argc, char *argv[])
 {
+	struct stat path_stat;
 	int fd;
 	int ret;
 
 	if (argc != 2) {
-		fprintf(stderr, "Use: %s <file path>\n", argv[0]);
+		fprintf(stderr, "Use: %s <file or dir path>\n", argv[0]);
 		return 1;
 	}
 
-	fd = open(argv[1], O_WRONLY, 0666);
-	if (fd == -1) {
-		perror("failed to open file");
+	ret = stat(argv[1], &path_stat);
+	if (ret == -1) {
+		perror("stat failed");
 		exit(1);
 	}
 
-	ret = unlink(argv[1]);
-	if (ret == -1) {
-		perror("unlink failed");
-		exit(2);
+	if (S_ISDIR(path_stat.st_mode))
+		fd = open(argv[1], O_RDONLY | O_DIRECTORY);
+	else
+		fd = open(argv[1], O_WRONLY, 0666);
+
+	if (fd == -1) {
+		perror("failed to open file/dir");
+		exit(1);
+	}
+
+	if (S_ISDIR(path_stat.st_mode)) {
+		ret = rmdir(argv[1]);
+		if (ret == -1) {
+			perror("rmdir failed");
+			exit(2);
+		}
+	} else {
+		ret = unlink(argv[1]);
+		if (ret == -1) {
+			perror("unlink failed");
+			exit(2);
+		}
 	}
 
 	ret = fsync(fd);
